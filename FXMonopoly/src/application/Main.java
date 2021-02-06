9e2562693d5a2f36a6f10931e03a84c100cdd83e
latifamouhdi <latifamouhdi123@gmail.com>
2021-02-06 14:20:46 +0100

    commit

--- FXMonopoly/src/application/Main.java
+++ FXMonopoly/src/application/Main.java
@@ -1,10 +1,34 @@
 package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.fxml.FXMLLoader;
 
-public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Main extends Application {
+	MediaPlayer MediaPlayer;
+	
+	@Override
+	public void start(Stage primaryStage) {
+//		Media music=new Media("/ressources/songs/ScottBuckley-Sleep.mp3");
+//		MediaPlayer=new MediaPlayer(music);
+//		MediaPlayer.setAutoPlay(true);
+//		MediaPlayer.setVolume(10);
+		try {
+			Parent root =FXMLLoader.load(getClass().getResource("/accueil/Accueil.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
-
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
